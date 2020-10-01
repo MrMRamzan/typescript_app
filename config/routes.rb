8789b60fc0651088e6636bbd651b38ea61e64ae8
mrmramzan <muhammad.ramzan@evoke.team>
2020-10-01 19:40:23 +0500

    Add post and comment models.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  root to: 'welcome#home'
+
+  root to: 'posts#index'
+  resources :posts
+  resources :comments
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
